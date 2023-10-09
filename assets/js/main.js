45a5ec5e7a0b7e814b21a304dd6f77a0fe979d01
Lucas Henrique <luhcashenrique@gmail.com>
2023-10-08 23:13:32 -0300

    update assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ function loadPokemon(event, id) {
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
-        <button onclick="loadPokemon(pokemon.number)">
+        <button onclick="loadPokemon(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
